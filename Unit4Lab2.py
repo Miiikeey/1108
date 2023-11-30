c2fad72ef84b4678e3ba9bd48ebe25f9cc932d73
Mike <mike.cabaluna@sait.edu.com>
2023-11-30 15:32:38 -0700

    Unit 4 Lab 2 complete

--- Unit4Lab2.py
+++ Unit4Lab2.py
@@ -1,15 +1,46 @@
 
-#class for bankaccount
-class Account:
-    #define the attributes
+
+class bankAccount:
+    """Constructor"""
     def __init__(self, accountNumber, customerName, balance):
-        self.number = accountNumber
-        self.name = customerName
+        self.accountNumber = accountNumber
+        self.customerName = customerName
         self.balance = balance
-    #define methods
-    def deposit(self, deposit):
-        self.deposit = deposit
-    def withdraw(self, withdraw):
-        self.withdraw = withdraw
-    def display(self, display):
-        self.display = display
+    
+    """Methods"""
+    def deposit(self, value):
+        self.balance += value
+        return f"New Balance ${self.balance}"
+    
+    def withdraw(self, value):
+        self.balance -= value
+        return f"New Balance ${self.balance}"
+    
+    def display(self):
+        return f"Account Number: {self.accountNumber}  Name: {self.customerName}  Balance: ${self.balance}"
+    
+
+number = int(input("Enter your acount number: "))
+name = input("Enter your name: ")
+balance = float(input("Enter your balance: "))
+
+account = bankAccount(number, name, balance)
+
+choice = int(input("Would you like to: \n 1. Deposit \n 2. Widthdraw \n 3. Display \n >"))
+
+if choice == 1:
+    value = int(input("How much would you like to deposit: "))
+    print(account.deposit(value))
+elif choice == 2:
+    value = int(input("How much would you like to withdraw: "))
+    print(account.withdraw(value))
+elif choice ==3:
+    print(account.display())
+else:
+    print("Invalid Input")
+
+
+
+
+
+
