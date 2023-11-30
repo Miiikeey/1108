926126c941115872f09745c075fdace43700d93b
Mike <mike.cabaluna@sait.edu.com>
2023-11-30 15:35:21 -0700

    comments and docstrings

--- Unit4Lab2.py
+++ Unit4Lab2.py
@@ -1,7 +1,7 @@
 
 
 class bankAccount:
-    """Constructor"""
+    """Constructor contains number, name and their balance"""
     def __init__(self, accountNumber, customerName, balance):
         self.accountNumber = accountNumber
         self.customerName = customerName
@@ -9,23 +9,28 @@ class bankAccount:
     
     """Methods"""
     def deposit(self, value):
+        """Add money to the balance"""
         self.balance += value
         return f"New Balance ${self.balance}"
     
     def withdraw(self, value):
+        """Subtract from the balance"""
         self.balance -= value
         return f"New Balance ${self.balance}"
     
     def display(self):
+        """Display all the information"""
         return f"Account Number: {self.accountNumber}  Name: {self.customerName}  Balance: ${self.balance}"
     
-
+#User inputs to insert inside the class
 number = int(input("Enter your acount number: "))
 name = input("Enter your name: ")
 balance = float(input("Enter your balance: "))
 
 account = bankAccount(number, name, balance)
 
+
+#choices for the methods
 choice = int(input("Would you like to: \n 1. Deposit \n 2. Widthdraw \n 3. Display \n >"))
 
 if choice == 1:
